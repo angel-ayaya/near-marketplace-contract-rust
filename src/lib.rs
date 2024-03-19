23171a003812e2b2371fcc2937f85d8a5cc5bc78
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2024-03-19 12:01:14 -0700

    Se agregaron los metodos init, set_product y get_product

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use near_sdk::collections::UnorderedMap;
+use near_sdk::store::UnorderedMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{near_bindgen, PanicOnDefault};
 
@@ -8,8 +8,23 @@ pub struct Marketplace {
      products: UnorderedMap<String, String>
 }
 
-
 #[near_bindgen]
 impl Marketplace {
-    // marketplace methods will be implemented here
+    
+    #[init]
+    pub fn init() -> Self {
+        Self {
+            products: UnorderedMap::new(b"product".to_vec()),
+        }
+    }
+
+    pub fn set_product(&mut self, id: String, product_name: String) {
+        self.products.insert(id, product_name);
+
+    }
+
+    pub fn get_product(&self, id: String) -> Option<String> {
+        self.products.get(&id).cloned()
+    }
+
 }
